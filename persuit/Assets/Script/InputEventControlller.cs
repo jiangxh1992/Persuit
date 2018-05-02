f6ad1438be7877262069b91fcd1ac21ed02e921c
jiangxh1992 <919575700@qq.com>
2018-05-02 20:25:54 +0800

    init

---
+++ persuit/Assets/Script/InputEventControlller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputEventControlller : Singleton<InputEventControlller> {
+
+    public delegate void InputInputDelegate();
+
+    public InputInputDelegate OnLeftDown;
+    public InputInputDelegate OnRightDown;
+    public InputInputDelegate OnLeftUp;
+    public InputInputDelegate OnRightUp;
+    public InputInputDelegate OnUp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            this.OnLeftDown();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            this.OnRightDown();
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            this.OnLeftUp();
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            this.OnRightUp();
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            this.OnUp();
+        }
+	}
+}
---
+++ persuit/Assets/Script/MainCharacter.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacter : Singleton<MainCharacter> {
+
+	// Use this for initialization
+    public float upForce = 500f;
+    public float horForce = 100f;
+    public float moveSpeed = 5.0f;
+    Rigidbody2D mRigidbody = null;
+    public HeaderProto.PCharState mCurState = HeaderProto.PCharState.PCharStateIdle;
+	void Start () {
+        moveSpeed = 5.0f;
+
+
+
+        mRigidbody = GetComponent<Rigidbody2D>();
+        InputEventControlller.Ins.OnUp += () =>
+        {
+            mRigidbody.velocity = Vector2.zero;
+            if (mCurState == HeaderProto.PCharState.PCharStateRun)
+            {
+                mRigidbody.AddForce((Vector2.up*2 + new Vector2(transform.right.x, transform.right.y)).normalized * upForce, ForceMode2D.Force);
+            }
+            else {
+                mRigidbody.AddForce(Vector2.up * upForce, ForceMode2D.Force);
+            }
+            mCurState = HeaderProto.PCharState.PCharStateJump;
+        };
+        InputEventControlller.Ins.OnLeftDown += () =>
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            mRigidbody.velocity = Vector2.zero;
+            if (mCurState == HeaderProto.PCharState.PCharStateJump) {
+                mRigidbody.AddForce(Vector2.left * horForce, ForceMode2D.Force);
+            }
+            else {
+                mCurState = HeaderProto.PCharState.PCharStateRun;
+            }
+        };
+        InputEventControlller.Ins.OnRightDown += () =>
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            mRigidbody.velocity = Vector2.zero;
+            if (mCurState == HeaderProto.PCharState.PCharStateJump)
+            {
+                mRigidbody.AddForce(Vector2.right * horForce, ForceMode2D.Force);
+            }
+            else
+            {
+                mCurState = HeaderProto.PCharState.PCharStateRun;
+            }
+        };
+        InputEventControlller.Ins.OnLeftUp += () =>
+        {
+            mCurState = HeaderProto.PCharState.PCharStateIdle;
+        };
+        InputEventControlller.Ins.OnRightUp += () =>
+        {
+            mCurState = HeaderProto.PCharState.PCharStateIdle;
+        };
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (mCurState == HeaderProto.PCharState.PCharStateRun) {
+            transform.Translate(new Vector2(1,0) * Time.deltaTime * moveSpeed);
+        }
+	}
+
+    void OnCollisionEnter(Collision collision) {
+        mCurState = HeaderProto.PCharState.PCharStateIdle;
+    }
+}
