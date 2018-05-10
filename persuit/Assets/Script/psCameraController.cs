913a501c86a9deff0a5c23263cd9a658ebb6c578
Tengyuamomo <many13795287611@qq.com>
2018-05-10 16:38:23 +0800

    动画资源-new

--- persuit/Assets/Script/psCameraController.cs
+++ persuit/Assets/Script/psCameraController.cs
@@ -14,10 +14,17 @@ public class psCameraController : MonoBehaviour {
 	void Update () {
         bool isCharacterActive = MainCharacter.Ins.mStateManager.mCurState == HeaderProto.PCharState.PCharStateJumpDown || MainCharacter.Ins.mStateManager.mCurState == HeaderProto.PCharState.PCharStateJumpUp || MainCharacter.Ins.mStateManager.mCurState == HeaderProto.PCharState.PCharStateRun;
         bool isInsideBorder = transform.position.y < cameraTopY && transform.position.y > cameraBottomY;
-        if (isCharacterActive && isInsideBorder)
-        {
-            transform.position = new Vector3(transform.position.x, MainCharacter.Ins.transform.position.y, transform.position.z);
-        }
-		
+		if (isCharacterActive && isInsideBorder) 
+		{
+			transform.position = new Vector3 (transform.position.x, MainCharacter.Ins.transform.position.y, transform.position.z);
+			if (this.transform.position.y > cameraTopY) 
+			{
+				this.transform.position = new Vector3 (transform.position.x, cameraTopY - 0.01f, transform.position.z);
+			} 
+			else if (this.transform.position.y < cameraBottomY) 
+			{
+				this.transform.position = new Vector3 (transform.position.x, cameraBottomY + 0.01f, transform.position.z);
+			}
+		}
 	}
 }
